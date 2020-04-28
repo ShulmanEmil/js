0d7ae6521193e23bf1fbd9aca172d470102511c7
Emil <shulman.emil@gmail.com>
2020-04-28 14:22:04 +0300

    fourth commit

--- script.js
+++ script.js
@@ -16,53 +16,58 @@ let appData = {
   expenses: {},
   optionalExpenses: {},
   income: [],
-  savings: true
+  savings: true,
+  chooseExpense: function() {
+    for (let i = 0; i < 2; i++) {
+      let consumption = prompt('Введите обязательную статью расходов в этом месяце');
+      let willCost = prompt('Во сколько обойдется?');
+    
+      if ( (typeof(consumption)) != null && (typeof(willCost)) != null ){
+      appData.expenses.consumption =  willCost;
+      } else {                            
+        console.log ("bad result");
+        i--;
+      }
+    }
+  },
+  chooseOptExpenses: function() {
+    for (let i = 0; i < 3; i++) {
+      let consumption = prompt('Введите НЕобязательную статью расходов в этом месяце');
+      let willCost = prompt('Во сколько обойдется?');
+    
+      if ( (typeof(consumption)) != null && (typeof(willCost)) != null ){
+      appData.optionalExpenses.i =  willCost;
+      } else {                            
+        console.log ("bad result");
+        i--;
+      }
+    }
+  },
+  detectDayBudget: function() {
+    alert( ((appData.budget - appData.expenses.consumption) / 30).toFixed(1) );
+  },
+  checkSavings: function() {
+    if (appData.savings == true) {
+      let save = +prompt("Какова сумма накоплений?"),
+          percent = +prompt("Под какой процент?");
+  
+      appData.monthIncone = (save / 100 / 12 * percent).toFixed(1);
+      alert(("Доход с депозита: " + appData.monthIncone) );
+    }
+  },
+  chooseIncome: function() {
+    let income;
+    while (income == "" || income == null) {
+      income = prompt("Доп. доходы? (Через запятую)");
+    }
+    appData.income = income.split(', ');
+    appData.income.sort();
+    appData.income.forEach(element => {
+      alert("Способы доп. заработка:" + element);
+    });
+  }
 };
 
-function chooseExpenses() {
-  for (let i = 0; i < 2; i++) {
-    let consumption = prompt('Введите обязательную статью расходов в этом месяце');
-    let willCost = prompt('Во сколько обойдется?');
-  
-    if ( (typeof(consumption)) != null && (typeof(willCost)) != null ){
-    appData.expenses.consumption =  willCost;
-    } else {                            
-      console.log ("bad result");
-      i--;
-    }
-  }
+for (const key in appData) {
+  alert("Наша программа включает в себя данные: " + key);
 }
-
-chooseExpenses();
-
-function chooseOptExpenses {
-  for (let i = 0; i < 3; i++) {
-    let consumption = prompt('Введите НЕобязательную статью расходов в этом месяце');
-    let willCost = prompt('Во сколько обойдется?');
-  
-    if ( (typeof(consumption)) != null && (typeof(willCost)) != null ){
-    appData.optionalExpenses.i =  willCost;
-    } else {                            
-      console.log ("bad result");
-      i--;
-    }
-  }
-}
-
-function detectDayBudget {
-  alert( ((appData.budget - appData.expenses.consumption) / 30).toFixed(1) );
-}
-
-detectDayBudget();
-
-function checkSavings() {
-  if (appData.savings == true) {
-    let save = +prompt("Какова сумма накоплений?"),
-        percent = +prompt("Под какой процент?");
-
-    appData.monthIncone = (save / 100 / 12 * percent).toFixed(1);
-    alert(("Доход с депозита: " + appData.monthIncone) );
-  }
-}
-
-checkSavings();
